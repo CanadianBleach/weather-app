ac52296e7904c03e58856cd513ac2240413cd343
Connor Baltich <45185643+CanadianBleach@users.noreply.github.com>
2023-11-22 17:32:12 -0500

    Boostrap Icons
    WE FANCY

---
+++ assets/pages/weather_page.js
@@ -0,0 +1,9 @@
+/* let lat = 0;
+let lon = 0;
+let key = '405cb79edbe390f6b9aaa5ccfb9e4af9';
+
+let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`;
+
+fetch(URL)
+  .then((response) => response.json())
+  .then((data) => console.log(data)); */
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,6 +2,6 @@ import WeatherTile from './weather_tile.js'
 
 let container = document.querySelector("#tile-parent");
 
-for (let i = 0; i < 4; i++) {
+/* for (let i = 0; i < 4; i++) {
     container.appendChild(new WeatherTile());
-}
+} */
